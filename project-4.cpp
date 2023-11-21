3a6989a30161dd8675d29cfd6b65f10b197c1204
playmakerdaniel <danielburrows12@gmail.com>
2023-11-21 10:03:26 -0800

    Third Commit

--- project-4.cpp
+++ project-4.cpp
@@ -68,3 +68,89 @@ bool nextPermutation(std::vector<int>& array) {
 
     return true;
 }
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+// Function prototypes
+template <typename T>
+bool nextPermutation(std::vector<T>& array);
+template <typename T>
+void displayArray(const std::vector<T>& array);
+template <typename T>
+void readArray(std::vector<T>& array, const std::string& input);
+bool isPermutationOf1234567(const std::vector<int>& array);
+
+// Main function
+int main() {
+    std::cout << "Enter 's' for string input or 'n' for numerical input: ";
+    char choice;
+    std::cin >> choice;
+    std::cin.ignore();  // Ignore newline after reading character
+
+    std::string input;
+    std::cout << "Enter the elements of the n-tuple: ";
+    std::getline(std::cin, input);
+
+    if (choice == 's') {
+        std::vector<char> array;
+        readArray(array, input);
+        if (nextPermutation(array)) {
+            displayArray(array);
+        } else {
+            std::cout << "No next permutation.\n";
+        }
+    } else if (choice == 'n') {
+        std::vector<int> array;
+        readArray(array, input);
+
+        if (!isPermutationOf1234567(array)) {
+            std::cout << "Error: Input is not a permutation of {1, 2, 3, 4, 5, 6, 7}.\n";
+            return 1;
+        }
+
+        if (nextPermutation(array)) {
+            displayArray(array);
+        } else {
+            std::cout << "No next permutation (last in lexicographic order).\n";
+        }
+    } else {
+        std::cout << "Invalid input type. Please restart the program.\n";
+    }
+
+    return 0;
+}
+
+// Function to read array from user input
+template <typename T>
+void readArray(std::vector<T>& array, const std::string& input) {
+    std::istringstream iss(input);
+    T element;
+    while (iss >> element) {
+        array.push_back(element);
+    }
+}
+
+// Function to display the array
+template <typename T>
+void displayArray(const std::vector<T>& array) {
+    for (const auto& elem : array) {
+        std::cout << elem << " ";
+    }
+    std::cout << std::endl;
+}
+
+// Function to check if the input is a permutation of {1, 2, 3, 4, 5, 6, 7}
+bool isPermutationOf1234567(const std::vector<int>& array) {
+    if (array.size() != 7) return false;
+    std::vector<bool> seen(7, false);
+    for (int num : array) {
+        if (num < 1 || num > 7 || seen[num - 1]) return false;
+        seen[num - 1] = true;
+    }
+    return true;
+}
+
+// [Function to generate the next permutation goes here, same as previous]
